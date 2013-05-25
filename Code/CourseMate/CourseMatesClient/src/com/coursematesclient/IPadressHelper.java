7b9d159316c0d7c69629551b170ccf1235450cc8
Ofir <ofirzh@gmail.com>
2013-05-25 03:55:33 +0300

    delete

--- Code/CourseMate/CourseMatesClient/src/com/coursematesclient/IPadressHelper.java
+++
@@ -1,7 +0,0 @@
-package com.coursematesclient;
-
-public class IPadressHelper {
-public String HOME_IP = "192.168.2.106";
-//public String HOME_IP = "10.12.66.70";
-//public String HOME_IP = "10.8.6.57";
-}
