ca693976821907f9a23a6a6a51de98bac49a708b
Ben <ben.ohana1@gmail.com>
2013-06-16 23:05:20 +0300

    fix rest ws

---
+++ Code/CourseMate/CourseMatesWS/CourseMatesREST.svc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using CourseMatesWS.DAL.Objects;
+using CourseMatesWS.BLL;
+using CourseMatesWS.DAL;
+
+namespace CourseMatesWS
+{
+    public class CourseMatesREST : ICourseMatesREST
+    {
+        public string LoginREST(string userName, string password, out int id)
+        {
+            return CMDal.LogIn(userName, password, out id);
+        }
+
+        public List<Course> GetCourseByUserIdREST(string sessionId, string userId)
+        {
+            int x;
+            int.TryParse(userId, out x);
+            return CMDal.GetCourseByUserId(sessionId, x);
+        }
+
+        public bool RegisterREST(User user, out int userId, out string sessionId)
+        {
+            sessionId = string.Empty;
+            SQLStatus status = CMDal.RegisterNewUser(user, out userId);
+
+            if (status == SQLStatus.Succeeded)
+            {
+                sessionId = CMDal.GetNewSession(userId);
+                return true;
+            }
+            return false;
+        }
+
+        public FileStructure GetCourseFilesREST(string sessionId, string userId, string courseId)
+        {
+            int cid, uid;
+            int.TryParse(courseId, out cid);
+            int.TryParse(userId, out uid);
+            return CMDal.GetCourseFiles(sessionId, uid, cid);
+        }
+
+        public List<User> GetCoursePartisipantREST(string sessionId, string userId, string courseId)
+        {
+            int cid, uid;
+            int.TryParse(courseId, out cid);
+            int.TryParse(userId, out uid);
+            return CMDal.GetCoursePartisipant(sessionId, uid, cid);
+        }
+
+        public Forum GetCourseForumREST(string sessionId, string userId, string courseId)
+        {
+            int cid, uid;
+            int.TryParse(courseId, out cid);
+            int.TryParse(userId, out uid);
+            return CMDal.GetCourseForum(sessionId, uid, cid);
+        }
+    }
+}
---
+++ Code/CourseMate/CourseMatesWS/ICourseMatesREST.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+using CourseMatesWS.DAL.Objects;
+
+namespace CourseMatesWS
+{
+    [ServiceContract]
+    public interface ICourseMatesREST
+    {
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, UriTemplate = "/login/{userName}/{password}", Method = "GET")]
+        string LoginREST(string userName, string password, out int id);
+
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, UriTemplate = "/GetCourseByUserId/{sessionId}/{userId}", Method = "GET")]
+        List<Course> GetCourseByUserIdREST(string sessionId, string userId);
+
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, UriTemplate = "/GetCourseFiles/{sessionId}/{userId}/{courseId}", Method = "GET")]
+        FileStructure GetCourseFilesREST(string sessionId, string userId, string courseId);
+
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, UriTemplate = "/GetCoursePartisipant/{sessionId}/{userId}/{courseId}", Method = "GET")]
+        List<User> GetCoursePartisipantREST(string sessionId, string userId, string courseId);
+
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, UriTemplate = "/GetCourseForum/{sessionId}/{userId}/{courseId}", Method = "GET")]
+        Forum GetCourseForumREST(string sessionId, string userId, string courseId);
+
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedResponse, RequestFormat = WebMessageFormat.Json, UriTemplate = "/Register", Method = "POST")]
+        bool RegisterREST(User stream, out int userId, out string sessionId);
+    }
+}
